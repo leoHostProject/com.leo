fc6c054a77d58ae2d6cd3b036ec66c7e0bb6ff3b
leo <leo_host@163.com>
2021-07-25 22:08:18 +0800

    hot fix first

---
+++ src/main/java/com/leo/App.java
@@ -0,0 +1,14 @@
+package com.leo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "this is hot-fix!" );
+    }
+}
