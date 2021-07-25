5302f2962f5d5734dc894b81da7114011c0f1388
leo <leo_host@163.com>
2021-07-25 22:13:35 +0800

    master first

--- src/main/java/com/leo/App.java
+++ src/main/java/com/leo/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "this is hot-fix!" );
+        System.out.println( "this is master and hot-fix!" );
     }
 }
